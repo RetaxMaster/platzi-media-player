833f2e97e36fcb2e3d09195e0112e0457691c1a4
RetaxMaster <retaxmaster@hotmail.com>
2020-11-20 20:28:44 -0600

    Adicion de los getters y setters

--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -1,8 +1,13 @@
 function AutoPlay() {}
 
 AutoPlay.prototype.run = function(player) {
-    player.mute();
+
+    if (!player.muted) {
+        player.muted = true;
+    }
+
     player.play();
+
 }
 
 export default AutoPlay;
