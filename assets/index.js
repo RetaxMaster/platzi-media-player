40e6020cbb602ec37b04a2f8b5200f929b8b8a8b
RetaxMaster <retaxmaster@hotmail.com>
2020-11-21 02:11:26 -0600

    Adicion de services workers

--- assets/index.js
+++ assets/index.js
@@ -19,3 +19,9 @@ const toggleMuteButton = document.querySelector('#toggleMute');
 
 togglePlay.onclick = () => player.togglePlay();
 toggleMuteButton.onclick = () => player.toggleMute();
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js").catch(error => {
+        console.log(error.messgae);
+    });
+}
---
+++ sw.js
@@ -0,0 +1,59 @@
+const VERSION = "v1";
+
+// self es como this para los services workers
+self.addEventListener("install", event => {
+
+    event.waitUntil(precache());
+
+});
+
+// Cada vez que se haga un request
+self.addEventListener("fetch", event => {
+
+    const request = event.request;
+
+    // solo con get
+    if (request.method !== "GET") {
+        return;
+    }
+
+    // buscar en caché
+    event.respondWith(cachedResponse(request));
+
+    // actualizar el cache (Para que puedan actualizarse los archivos en caso de haber actualizaciones)
+    event.waitUntil(updateCache(request));
+
+});
+
+async function precache() {
+    const cache = await caches.open(VERSION);
+
+    // Añadimos los recursos
+    return cache.addAll([
+      '/',
+      '/index.html',
+      '/assets/index.js',
+      '/assets/MediaPlayer.js',
+      '/assets/plugins/AutoPlay.js',
+      '/assets/plugins/AutoPause.js',
+      '/assets/index.css',
+      '/assets/BigBuckBunny.mp4',
+    ]);
+}
+
+async function cachedResponse(request) {
+
+    const cache = await caches.open(VERSION);
+    const response = await cache.match(request); // <- Puede dar undefined si es que no existe en caché
+
+    return response || fetch(request);
+    
+}
+
+async function updateCache(request) {
+
+    const cache = await caches.open(VERSION);
+    const response = await fetch(request);
+    return cache.put(request, response);
+    
+}
